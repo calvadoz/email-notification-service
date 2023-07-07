4bebb1dbbdff48f3a3009915b6ac92e5e022f343
Kelvin Wu <wu.kelvin91@gmail.com>
2023-07-07 23:52:46 +0800

    chore: setup data model schema, remove markdown

--- src/controller/hookController.ts
+++ src/controller/hookController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express'
+import RequestModel from '../model/Request'
 
 const hookRequestHandler = async (req: Request, res: Response) => {
-  console.log(req.body)
-  res.sendStatus(200)
+  try {
+    const { payload } = req.body
+    if (!payload) {
+      res.sendStatus(400)
+      return
+    }
+    res.sendStatus(200)
+  } catch (err) {
+    console.error('Error handling incoming request: ', err)
+    res.sendStatus(500)
+  }
 }
 
 export default { hookRequestHandler }
