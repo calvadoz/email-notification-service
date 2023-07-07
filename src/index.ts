97ac7d48a37d1fba3c37ea255cab86e118336a43
Kelvin Wu <wu.kelvin91@gmail.com>
2023-07-07 22:37:58 +0800

    chore: add default route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import { Request, Response } from 'express'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendStatus(200)
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
 
